c0498f8e4d61af85b573787194d4249807f09506
fabioir <fabioiglesias@hotmail.es>
2018-08-01 08:26:39 +0200

    Not a suitable option fot Angular

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import Grid from 'tui-grid';
-import { timeout } from '../../../node_modules/@types/q';
+
+import { DmnService } from '../dmn.service';
 
 @Component({
   selector: 'app-grid',
@@ -11,8 +12,11 @@ import { timeout } from '../../../node_modules/@types/q';
 export class GridComponent implements OnInit {
 
   instance;
+  decisionTable;
 
-  constructor() { }
+  constructor(
+    private dmnService: DmnService
+  ) { }
 
   ngOnInit() {
 
@@ -21,269 +25,79 @@ export class GridComponent implements OnInit {
       el: ('#grid'), // Container element
       columns: [
         {
-          title: 'Name',
-          name: 'name'
-        },
-        {
-          title: 'Artist',
-          name: 'artist'
-        },
-        {
-          title: 'Release',
-          name: 'release'
-        },
-        {
-          title: 'Genre',
-          name: 'genre'
-        },
-        {
-          title: 'Name',
-          name: 'name'
-        },
-        {
-          title: 'Artist',
-          name: 'artist'
-        },
-        {
-          title: 'Release',
-          name: 'release'
-        },
-        {
-          title: 'Genre',
-          name: 'genre'
-        },
-        {
-          title: 'Name',
-          name: 'name'
-        },
-        {
-          title: 'Artist',
-          name: 'artist'
-        },
-        {
-          title: 'Release',
-          name: 'release'
-        },
-        {
-          title: 'Genre',
-          name: 'genre'
-        },
-        {
-          title: 'Name',
-          name: 'name'
-        },
-        {
-          title: 'Artist',
-          name: 'artist'
-        },
-        {
-          title: 'Release',
-          name: 'release'
-        },
-        {
-          title: 'Genre',
-          name: 'genre'
+          title: 'Number',
+          name: 'number'
         }
       ],
       data: [
         {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
-        },
-        {
-          name: 'Beautiful Lies',
-          artist: 'Birdy',
-          release: '2016.03.26',
-          genre: 'Pop'
+          number: '1'
         }
       ]
     });
 
     Grid.applyTheme('striped');
 
-    console.log(this.instance)
+    this.decisionTable = this.dmnService.parse();
   }
 
+  launch() {
+    //console.log(this.dmnService.response);
+    this.decisionTable = this.dmnService.response.drgElements[0].decisionTable;
+    console.log(this.decisionTable)
+
+    //Extract grid columns
+    const columns = this.instance.getColumns();
+
+    //Push inputs
+    let inputCount = 0;
+    this.decisionTable.input.forEach(input => {
+      columns.push({
+        title: `Input${inputCount}${input.id}`,
+        name: `input${inputCount}`
+      });
+      inputCount++;
+    });
+
+    //Push outputs
+    let outputCount = 0;
+    this.decisionTable.output.forEach(output => {
+      columns.push({
+        title: `Output${outputCount}${output.id}`,
+        name: `output${outputCount}`
+      });
+      outputCount++;
+    });
+
+    this.instance.setColumns(columns);
+
+    //Lets go for the data
+    const rows = new Array();
+    let rowCount = 0;
+    this.decisionTable.rule.forEach(rule => {
+      const row = {};
+
+      row['number'] = rowCount;
+      //Input entries
+      let counter = 0;
+      rule.inputEntry.forEach(inputEntry => {
+        row[`input${counter}`] = inputEntry.text;
+        counter++;
+      });
+
+      //Output entries
+      counter = 0;
+      rule.outputEntry.forEach(outputEntry => {
+        row[`output${counter}`] = outputEntry.text;
+        counter++;
+      });
+
+      rows.push(row);
+      rowCount++;
+    });
+    this.instance.setData(rows);
+    console.log(this.instance.getRows());
+    console.log(this.instance.getColumns());
+
+  }
 }
